f3d4e93a7295513f8027a30d6368c40c3e118619
WangyanFR <115609054+WangyanFR@users.noreply.github.com>
2025-03-21 22:22:10 +0800

    Add files via upload

---
+++ PPO_continuous_main.py
@@ -0,0 +1,128 @@
+import torch
+import numpy as np
+import gym
+import argparse
+from normalization import Normalization, RewardScaling
+from replaybuffer import ReplayBuffer
+from ppo_continuous import PPO_continuous
+
+
+def main(args, env_name, seed):
+    env = gym.make(env_name)
+    env_evaluate = gym.make(env_name)  # When evaluating the policy, we need to rebuild an environment
+    # Set random seed
+    env.action_space.seed(seed)
+    env_evaluate.action_space.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    args.state_dim = env.observation_space.shape[0]
+    args.action_dim = env.action_space.shape[0]
+    args.max_action = 0.04
+    args.max_episode_steps = 80  # Maximum number of steps per episode
+    print("env={}".format(env_name))
+    print("state_dim={}".format(args.state_dim))
+    print("action_dim={}".format(args.action_dim))
+    print("max_action={}".format(args.max_action))
+    print("max_episode_steps={}".format(args.max_episode_steps))
+
+    evaluate_num = 0  # Record the number of evaluations
+    evaluate_rewards = []  # Record the rewards during the evaluating
+    total_steps = 6400  # Record the total steps during the training
+
+    replay_buffer = ReplayBuffer(args)
+    agent = PPO_continuous(args)
+
+    weight_path1 = './weight/actor_weights_step6400.pth'  # 设置正确的权重文件路径
+    weight_path2 = './weight/critic_weights_step6400.pth'  # 设置正确的权重文件路径
+    agent.actor.load_state_dict(torch.load(weight_path1))
+    agent.critic.load_state_dict(torch.load(weight_path2))
+
+    state_norm = Normalization(shape=args.state_dim)  # Trick 2:state normalization
+    if args.use_reward_norm:  # Trick 3:reward normalization
+        reward_norm = Normalization(shape=1)
+    elif args.use_reward_scaling:  # Trick 4:reward scaling
+        reward_scaling = RewardScaling(shape=1, gamma=args.gamma)
+
+    while total_steps < args.max_train_steps:
+        s = env.reset()
+        if args.use_state_norm:
+            s = state_norm(s)
+        if args.use_reward_scaling:
+            reward_scaling.reset()
+        episode_steps = 0
+        done = False
+        while not done:
+            episode_steps += 1
+            a, a_logprob = agent.choose_action(s)  # Action and the corresponding log probability
+            if args.policy_dist == "Beta":
+                action = a * args.max_action # [0,1]->[0,max]
+            else:
+                action = a
+            s_, r, done, truncated, info = env.step(action)
+
+            if args.use_state_norm:
+                s_ = state_norm(s_)
+            if args.use_reward_norm:
+                r = reward_norm(r)
+            elif args.use_reward_scaling:
+                r = reward_scaling(r)
+
+            # When dead or win or reaching the max_episode_steps, done will be Ture, we need to distinguish them;
+            # dw means dead or win,there is no next state s';
+            # but when reaching the max_episode_steps,there is a next state s' actually.
+            dw = False
+
+            # Take the 'action'，but store the original 'a'（especially for Beta）
+            replay_buffer.store(s, a, a_logprob, r, s_, dw, done)
+            s = s_
+            total_steps += 1
+
+            # When the number of transitions in buffer reaches batch_size,then update
+            if replay_buffer.count == args.batch_size:
+                agent.update(replay_buffer, total_steps)
+                replay_buffer.count = 0
+
+            if total_steps % args.save_freq == 0:
+                # np.save('./data_train/PPO_continuous_{}_env_{}__seed_{}.npy'.format(args.policy_dist, env_name, seed), np.array(evaluate_rewards))
+                # 保存Actor的权重
+                torch.save(agent.actor.state_dict(), './weight/actor_weights_step{}.pth'.format(total_steps))
+                # 保存Critic的权重
+                torch.save(agent.critic.state_dict(), './weight/critic_weights_step{}.pth'.format(total_steps))
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Hyperparameters Setting for PPO-continuous")
+    parser.add_argument("--max_train_steps", type=int, default=int(16000), help=" Maximum number of training steps")
+    parser.add_argument("--max_train_episodes", type=int, default=int(200), help=" Maximum number of training steps")
+    parser.add_argument("--evaluate_freq", type=float, default=8000, help="Evaluate the policy every 'evaluate_freq' steps")
+    parser.add_argument("--save_freq", type=int, default=800, help="Save frequency")
+    parser.add_argument("--policy_dist", type=str, default="Beta", help="Beta or Gaussian")
+    parser.add_argument("--batch_size", type=int, default=160, help="Batch size")
+    parser.add_argument("--mini_batch_size", type=int, default=64, help="Minibatch size")
+    parser.add_argument("--hidden_width", type=int, default=512, help="The number of neurons in hidden layers of the neural network")
+    parser.add_argument("--lr_a", type=float, default=1e-3, help="Learning rate of actor")
+    parser.add_argument("--lr_c", type=float, default=1e-3, help="Learning rate of critic")
+    parser.add_argument("--gamma", type=float, default=0.97, help="Discount factor")
+    parser.add_argument("--lamda", type=float, default=0.95, help="GAE parameter")
+    parser.add_argument("--epsilon", type=float, default=0.2, help="PPO clip parameter")
+    parser.add_argument("--K_epochs", type=int, default=20, help="PPO parameter")
+    parser.add_argument("--use_adv_norm", type=bool, default=False, help="Trick 1:advantage normalization")
+    parser.add_argument("--use_state_norm", type=bool, default=False, help="Trick 2:state normalization")
+    parser.add_argument("--use_reward_norm", type=bool, default=False, help="Trick 3:reward normalization")
+    parser.add_argument("--use_reward_scaling", type=bool, default=False, help="Trick 4:reward scaling")
+    parser.add_argument("--entropy_coef", type=float, default=0.01, help="Trick 5: policy entropy")
+    parser.add_argument("--use_lr_decay", type=bool, default=True, help="Trick 6:learning rate Decay")
+    parser.add_argument("--use_grad_clip", type=bool, default=True, help="Trick 7: Gradient clip")
+    parser.add_argument("--use_orthogonal_init", type=bool, default=True, help="Trick 8: orthogonal initialization")
+    parser.add_argument("--set_adam_eps", type=float, default=True, help="Trick 9: set Adam epsilon=1e-5")
+    parser.add_argument("--use_tanh", type=float, default=True, help="Trick 10: tanh activation function")
+
+    args = parser.parse_args()
+
+    env_name = 'CylinderEnv2_False-v0'
+    main(args, env_name=env_name, seed=10086)
+    # test(args, env_name=env_name, seed=12315)
+
